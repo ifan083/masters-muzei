5a7006a09f295571b48d2d3e1527d4ce1ae15000
ifan.ilievski@gmail.com <ifan.ilievski@gmail.com@7ead5648-3c44-a233-0249-7b422a08cb23>
2014-06-22 23:20:26 +0000

    added photo crop implementation

--- TextRecoApp/src/com/example/textrecoapp/MainActivity.java
+++ TextRecoApp/src/com/example/textrecoapp/MainActivity.java
@@ -1,85 +1,117 @@
 package com.example.textrecoapp;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 
 import android.app.Activity;
-import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
+import android.hardware.Camera.ShutterCallback;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceHolder.Callback;
 import android.view.SurfaceView;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.textrecoapp.ar.TrainSetHandler;
+import com.googlecode.tesseract.android.TessBaseAPI;
 
 public class MainActivity extends Activity implements Callback {
 
-  public static final String DATA_PATH = Environment.getExternalStorageDirectory().toString() + "/TextRecoApp";
-  public static final String LANG = "mkd";
+  // paths
+  public static final String PHOTO_PATH = Environment.getExternalStorageDirectory().toString()
+      + "images/dummy_photo.jpg";
+
+  // settings
   public static final String PHOTO_TAKEN = "photo taken";
   public static final String LOG_TAG = "MainActivity";
 
   private Camera camera;
 
+  // ui
+  private SurfaceView surfaceView;
+  private View scanArea;
+  private Button scanBtn;
+  private ImageView scanResult;
+
+  // OCR
+  private TessBaseAPI ocrAPI;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    initDirectory();
+
+    TrainSetHandler tsh = new TrainSetHandler(this, "mkd");
+    tsh.initDirectory();
+
+    new Runnable() {
+
+      @Override
+      public void run() {
+        initOCR_API();
+      }
+    }.run();;
+
     setContentView(R.layout.activity_main);
     initUI();
   }
 
-  private void initDirectory() {
-    String[] paths = new String[]{DATA_PATH, DATA_PATH + "/tessdata/"};
-    for (String path : paths) {
-      File dir = new File(path);
-      if (!dir.exists()) {
-        if (!dir.mkdirs()) {
-          Log.e(LOG_TAG, "ERROR: Creation of directory " + path + " on SD Card failed");
-          return;
-        } else {
-          Log.v(LOG_TAG, "Created directory " + path + " on SD Card");
-        }
-      }
-    }
-
-    checkAndCreateTrainedData();
-  }
-
-  private void checkAndCreateTrainedData() {
-    if (!(new File(DATA_PATH + "/tessdata/" + LANG + ".traineddata")).exists()) {
-      try {
-        AssetManager assetsManager = getAssets();
-        InputStream is = assetsManager.open(LANG + ".traineddata");
-        OutputStream os = new FileOutputStream(DATA_PATH + "/tessdata/" + LANG + "traineddata");
-
-        byte[] buffer = new byte[1024];
-        int lenght;
-
-        while ((lenght = is.read(buffer)) > 0) {
-          os.write(buffer, 0, lenght);
-        }
-
-        is.close();
-        os.close();
-
-        Log.v(LOG_TAG, "Copied " + LANG + ".traineddata");
-      } catch (Exception e) {
-        Log.e(LOG_TAG, "Was unable to copy " + LANG + ".traineddata");
-      }
-    }
+  private void initOCR_API() {
+    ocrAPI = new TessBaseAPI();
+    ocrAPI.init(TrainSetHandler.DATA_PATH, "mkd");
   }
 
   private void initUI() {
-    SurfaceView surface = (SurfaceView) findViewById(R.id.surface_view);
+    surfaceView = (SurfaceView) findViewById(R.id.surface_view);
 
-    SurfaceHolder holder = surface.getHolder();
+    SurfaceHolder holder = surfaceView.getHolder();
     holder.addCallback(this);
-    holder.setFixedSize(400, 300);
+
+    scanArea = findViewById(R.id.scan_area);
+    scanBtn = (Button) findViewById(R.id.scan_btn);
+    scanResult = (ImageView) findViewById(R.id.result_img);
+
+    final int scanAreaWidth = getResources().getDimensionPixelOffset(R.dimen.scan_area_width);
+    final int scanAreaHeight = getResources().getDimensionPixelOffset(R.dimen.scan_area_height);
+    scanBtn.setOnClickListener(new OnClickListener() {
+
+      @Override
+      public void onClick(View v) {
+        camera.takePicture(null, null, new PictureCallback() {
+
+          @Override
+          public void onPictureTaken(byte[] data, Camera camera) {
+            Bitmap source = BitmapFactory.decodeByteArray(data, 0, data.length);
+
+            float wfact = source.getWidth() / (float) surfaceView.getWidth();
+            float hfact = source.getHeight() / (float) surfaceView.getHeight();
+
+            int picScanWidth = (int) (wfact * scanAreaWidth);
+            int picScanHeight = (int) (hfact * scanAreaHeight);
+
+            int x = source.getWidth() / 2 - picScanWidth / 2;
+            int y = source.getHeight() / 2 - picScanHeight / 2;
+
+            Bitmap bmp = Bitmap.createBitmap(source, x, y, picScanWidth, picScanHeight);
+            source.recycle();
+
+            scanResult.setImageBitmap(bmp);
+
+            ocrAPI.setImage(bmp);
+            String recongizedtext = ocrAPI.getUTF8Text();
+            Toast.makeText(MainActivity.this, recongizedtext, Toast.LENGTH_SHORT).show();
+          }
+        });
+
+      }
+    });
   }
 
   @Override
